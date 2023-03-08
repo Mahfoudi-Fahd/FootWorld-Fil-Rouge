29f89a333c98ddf1db1f0dbbc704a9252c420094
FAHD <mahfoudifahd55@gmail.com>
2023-03-08 09:41:56 +0100

    phone number and adress added to customers table

--- database/migrations/2023_03_08_083453_create_admins_table.php
+++ database/migrations/2023_03_08_083453_create_admins_table.php
@@ -13,15 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('customers', function (Blueprint $table) {
+        Schema::create('admins', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('phone_number')->nullable();
-            $table->string('address')->nullable();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('username')->unique();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('email')->unique();
             $table->timestamps();
         });
     }
@@ -33,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('customers');
+        Schema::dropIfExists('admins');
     }
 };
