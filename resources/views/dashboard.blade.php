ebe82a929c0da6e682ceed8e674e41b4ddeed04d
FAHD <mahfoudifahd55@gmail.com>
2023-03-09 11:17:56 +0100

    authentication by jetstreem

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,14 @@
+<link rel="stylesheet" href={{url('css/style.css')}}>
+<link href='https://fonts.googleapis.com/css?family=Clicker Script' rel='stylesheet'>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
+
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    
+</x-app-layout>
