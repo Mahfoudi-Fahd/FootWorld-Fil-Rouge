b99635a73092be2c23e75cb931f11aaa5de70ca3
FAHD <mahfoudifahd55@gmail.com>
2023-04-02 14:43:37 +0000

    creating items seeder

---
+++ app/Models/Item.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Item extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'category_id',
+        'image',
+        'price',
+        'status',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ database/seeders/ItemSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use App\Models\Item;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class ItemSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Get all categories and users
+        $categories = Category::all();
+        $users = User::all();
+
+        // Seed 50 items
+        for ($i = 1; $i <= 50; $i++) {
+            Item::create([
+                'name' => "Item $i",
+                'description' => "Description for item $i",
+                'category_id' => $categories->random()->id,
+                'image' => "image$i.jpg",
+                'price' => rand(10, 100),
+                'status' => 'available',
+                // 'user_id' => $users->random()->id,
+            ]);
+        }
+    }
+}
