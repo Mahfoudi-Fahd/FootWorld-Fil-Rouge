5c03e5138d1993cc0cd1d2998006d40df77ff006
FAHD <mahfoudifahd55@gmail.com>
2023-04-27 16:38:29 +0100

    edit checkout page error

--- resources/views/show-product.blade.php
+++ resources/views/show-product.blade.php
@@ -8,57 +8,7 @@
 
 
 @include('components.landing-nav')
-{{-- <nav class="ms-lg-5 navbar navbar-expand-lg">
-    <div class="container-fluid d-flex justify-content-between">
-        <div class="d-flex mx-5">
-             <a class="ms-lg-5 navbar-brand" href="#">Foot Woorld</a>
-        </div>
-       
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse ms-5" id="navbarNav">
-            <div class="d-flex m-auto">
-                 <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{url('/')}}">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">About Us</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('items.show') }}">Products</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{route('contact-us.view')}}">Contact Us</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{url('showcart')}}"><i class='bx bxs-cart'></i> Cart </a>
-                    </li>
-                </ul>
-            </div>
 
-
-
-            <div class="">
-                @if (Route::has('login'))
-                    <div class="d-flex m-auto ">
-                        @auth
-                            <a href="{{ url('/dashboard') }}" class="text-sm text-dark">Dashboard</a>
-                        @else
-                            <a href="{{ route('login') }}" class="btn signin-button">Log in</a>
-    
-                            @if (Route::has('register'))
-                                <a href="{{ route('register') }}" class="btn signup-button">Register</a>
-                            @endif
-                        @endauth
-                    </div>
-                @endif
-            </div>
-        </div>
-    </div>
-
-</nav>  --}}
 
 
 <div class="row mt-5">
