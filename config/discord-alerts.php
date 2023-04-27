7fb2fc9203e339135cbda7701e936f44885e47b6
FAHD <mahfoudifahd55@gmail.com>
2023-04-27 17:00:04 +0100

    start adding discord webhook

---
+++ config/discord-alerts.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    /*
+     * The webhook URLs that we'll use to send a message to Discord.
+     */
+    'webhook_urls' => [
+        'default' => env('DISCORD_ALERT_WEBHOOK'),
+        'marketing' => 'https://discord.com/api/webhooks/1100711155682525254/7mfmDs1BVIzMtaQ6RJ8BTyYZELxRFO2gI1O--doY-lZP8rHe_n1jHVZdogR5Wvp8lpfj',
+    ],
+
+    /*
+     * This job will send the message to Discord. You can extend this
+     * job to set timeouts, retries, etc...
+     */
+    'job' => Spatie\DiscordAlerts\Jobs\SendToDiscordChannelJob::class,
+];
